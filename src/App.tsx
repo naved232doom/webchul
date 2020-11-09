08132b7404571dfc7de4d3361e14246a3bdb2ba8
naved232 <65990919+naved232doom@users.noreply.github.com>
2020-11-09 19:45:47 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda,
+EventSettingsModel} from '@syncfusion/ej2-react-schedule';
 
-function App() {
+import {DataManager, WebApiAdaptor} from '@syncfusion/ej2-data';
+
+class App extends React.Component {
+  private localData: EventSettingsModel ={
+    dataSource: [{
+      EndTime : new Date(2020, 10, 11, 6, 30),
+      StartTime : new Date(2020,10,11,4,0)
+    }]
+  };
+  private remoteData = new DataManager({
+    url :'https://js.syncfusion.com/demos/ejservices/api/Schedule/LocalData',
+    adaptor: new WebApiAdaptor,
+    crossDomain: true
+  });
+  public render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <React.Fragment>
+     
+      <ScheduleComponent currentView='Month' selectedDate={new Date(2020,10,11)}  
+      eventSettings= {{dataSource: this.remoteData}}>
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]}></Inject>
+    </ScheduleComponent>
+    </React.Fragment>
+    
   );
+  }
 }
 
 export default App;
